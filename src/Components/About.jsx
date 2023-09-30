470247918becae3de30c6780a7d08d8ee705aaab
SimplyReal <sianasharma37@gmail.com>
2023-09-30 21:47:47 +0530

    commit changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/design-desk.jpeg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CSE(Business System) undergraduate student studying at Techno Main Salt Lake. I enjoy exploring tech and its related fields.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C/C++, Python",
+  "DSA",
+  "Object Oriented Programming",
+  "Oracle(SQL)",
+  "Web Development",
+  "Exploring Machine Learning",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in an innovative way. Additionally, I love mathematics so I worked as a Subject Matter Expert in online platforms.";
 
 const About = () => {
   return (
